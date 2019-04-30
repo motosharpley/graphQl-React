d214d5ae33397e27bcd81d7b3e7034d97f3542da
Bryan <motosharpley@gmail.com>
2019-04-30 12:17:17 -0500

    refactor to class component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
 
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
