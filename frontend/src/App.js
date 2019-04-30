1d38f17884106c5436264c2bd55b85df12ce8a26
Bryan <motosharpley@gmail.com>
2019-04-30 12:55:19 -0500

    add router and base pages/components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
+import BookingsPage from './pages/Bookings';
+import EventsPage from './pages/Events';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Redirect from="/" to="/auth" exact/>
+          <Route path="/auth" component={AuthPage} />
+          <Route path="/events" component={EventsPage} />
+          <Route path="/bookings" component={BookingsPage} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
