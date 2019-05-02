34c91996117df4c45783505414ed9f21ecbcfc5c
Bryan <motosharpley@gmail.com>
2019-05-02 11:09:52 -0500

    add login form

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 
+import './Auth.css';
+
 class AuthPage extends Component {
   render() {
-    return <h1>AuthPage</h1>;
+    return (
+      <form className="auth-form">
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <div className="form-actions">
+          <button type="submit">Sign In</button>
+          <button type="button">Go To Sign Up</button>
+        </div>
+      </form>
+    );
   }
 }
 
