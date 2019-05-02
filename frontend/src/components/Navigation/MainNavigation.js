1a7b3a8223183817ad2be8eadced1befd2d0622b
Bryan <motosharpley@gmail.com>
2019-05-02 10:33:50 -0500

    add initial navbar styling

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import './MainNavigation.css';
+
 const mainNavigation = props => (
-  <header>
+  <header className="main-navigation">
     <div className="main-navigation_logo">
       <h1>React Events</h1>
     </div>
-    <nav className="main-navigation_item">
+    <nav className="main-navigation_items">
       <ul>
         <li><NavLink to="/auth">Authentication</NavLink></li>
         <li><NavLink to="/events">Events</NavLink></li>
