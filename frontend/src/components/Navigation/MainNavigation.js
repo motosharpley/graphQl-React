8883e99a27a6ec8a165470444bb774f915cb5930
Bryan <motosharpley@gmail.com>
2019-05-09 12:10:39 -0500

    add authContext to navigation

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import AuthContext from '../../context/auth-context';
 import './MainNavigation.css';
 
 const mainNavigation = props => (
-  <header className="main-navigation">
-    <div className="main-navigation_logo">
-      <h1>React Events</h1>
-    </div>
-    <nav className="main-navigation_items">
-      <ul>
-        <li><NavLink to="/auth">Authentication</NavLink></li>
-        <li><NavLink to="/events">Events</NavLink></li>
-        <li><NavLink to="/bookings">Bookings</NavLink></li>
-      </ul>
-    </nav>
-  </header>
+  <AuthContext.Consumer>
+    {(context) => {
+      return (
+        <header className="main-navigation">
+          <div className="main-navigation_logo">
+            <h1>React Events</h1>
+          </div>
+          <nav className="main-navigation_items">
+            <ul>
+              {!context.token && (
+                <li>
+                  <NavLink to="/auth">Authentication</NavLink>
+                </li>
+              )}
+              <li>
+                <NavLink to="/events">Events</NavLink>
+              </li>
+              {context.token && (
+                <li>
+                  <NavLink to="/bookings">Bookings</NavLink>
+                </li>
+              )}
+            </ul>
+          </nav>
+        </header>
+      )
+    }}
+
+  </AuthContext.Consumer>
 );
 
 export default mainNavigation;
